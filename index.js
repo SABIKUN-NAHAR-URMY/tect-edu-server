44f76eecb600c6074b260503e00f4624a5a4ae63
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-10-27 01:53:56 +0600

    courses json file update

--- index.js
+++ index.js
@@ -5,24 +5,19 @@ const port = process.env.Port || 5000;
 
 app.use(cors());
 
-const coursesData = require('./data/courses.json');
-const courseDetails = require('./data/CoursesDetails.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/courses', (req, res) => {
-    res.send(coursesData);
+    res.send(courses);
 })
 
-app.get('/coursesDetail', (req, res) => {
-  res.send(courseDetails);
-})
-
-app.get('/coursesDetail/:id', (req, res) => {
+app.get('/courses/:id', (req, res) => {
   const id = req.params.id;
-  const selectedDetails = courseDetails.filter(cd => cd.courses_id === id);
+  const selectedDetails = courses.filter(c => c.id == id);
   res.send(selectedDetails);
 })
 
